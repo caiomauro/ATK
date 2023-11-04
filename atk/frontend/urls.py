0de1bb5dffbe478705a0cf8254f1cc468aabb4b9
Caio Mauro <101437705+caiomauro@users.noreply.github.com>
2023-11-03 22:51:36 -0400

    Update v1.1.1
    Done:
    -Delete functionality added for staff

--- atk/frontend/urls.py
+++ atk/frontend/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='frontend/login.html'), name="login"),
     path('login/', auth_views.LoginView.as_view(template_name='frontend/login.html'), name="login"),
     path('home/', views.home, name="home"),
-    path('createpost/', views.createpost, name="createpost")
+    path('createpost/', views.createpost, name="createpost"),
+    path('deletepost/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
--- atk/frontend/views.py
+++ atk/frontend/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 from api.models import ForumPost
 
 # Create your views here.
 @login_required
 def home(request, *args, **kwargs):
-    return render(request, 'frontend/home.html')
+    if request.user.is_authenticated:
+        forum_posts = ForumPost.objects.all()
+        return render(request, 'frontend/home.html', {'forum_posts': forum_posts})
+    else:
+        return render(request, 'frontend/home.html')
 
 def login(request, *args, **kwargs):
     return render(request, 'frontend/login.html')
@@ -23,3 +28,16 @@ def createpost(request, *args, **kwargs):
         new_post.save()
         return redirect('/home')
     return render(request, 'frontend/createpost.html')
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(ForumPost, id=post_id)
+    if request.method == 'POST':
+        if request.user.is_staff:
+            post.delete()
+            messages.success(request, 'Post deleted successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, 'You do not have permission to delete this post.')
+            return redirect('home')
+    return render(request, 'frontend/delete_post.html', {'post': post})
